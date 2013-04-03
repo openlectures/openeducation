a72483cb417c704ee01cd0b9e241131c76d95b59
Jethro Kuan <jethrokuan95@gmail.com>
2013-04-03 15:50:31 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openeducation::Application.config.secret_key_base = 'e240fa33dda3276d7813991ab38e044521b7bd7be791e938364603e33a0dda84adb1a0693665511f2335d72b3b9d55cf65427015a14f0b03ac88ee3ffaad016c'
